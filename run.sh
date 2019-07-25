5c4cd35d6458c5f5fe2b6f43adc4c3881cc347e1
Mark Meyer <mark@ofosos.org>
2019-07-25 13:16:08 +0200

    add QT_IM_MODULE for on-screen keyboard

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 export LD_LIBRARY_PATH=/gnu/store/69x60a1pn0mf5jv68al8awjfkyp1miwi-gcc-8.3.0-lib/lib/:/gnu/store/xj4wllkddsnlnqqv0cbracbqc03r4f9i-mesa-19.1.1/lib/:/gnu/store/nq4lsyipmfb0q7g26ra45rwwqrh3x8zw-zlib-1.2.11/lib/:/gnu/store/0q9pq9flr76rh4bv2524niknknnl2kvq-glib-2.56.3/lib/:/gnu/store/hnj08aavzpsi72wvi4qqrp5dqbyqxqqs-libxcb-1.13/lib/:/gnu/store/8baabfjazsr7s4y0jig1sn84xnxf75xa-libx11-1.6.6/lib/:/gnu/store/iya2ks1vgp9vkcfg0lq2l4dc7cnbhmn9-fontconfig-2.13.1/lib/:/gnu/store/66db1c64qm8ar4d7qjxsyl3xvjqjpgj7-freetype-2.9.1/lib/:/gnu/store/1wgjfp47da8zm7ap9n0sl6wfn295qvcw-libxi-1.7.9/lib/:/gnu/store/zjalyyj5v1gygqh613p3y5b3hp7rdxpj-dbus-1.12.12/lib/:/gnu/store/xrvwszmahcb7k2zcyag3vmqwswzrbvcg-libxrender-0.9.10/lib/
 unset QT_PLUGIN_PATH
+export QT_IM_MODULE=qtvirtualkeyboard
 python3 strichliste.py
