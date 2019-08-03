63a70afd6dcffa929458def96ebebdf6454daf24
Mark Meyer <mark@ofosos.org>
2019-08-03 16:02:48 +0200

    Add first version of tagutil.py

---
+++ tagutil.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from pirc522 import RFID
+import signal
+import time
+
+rdr = RFID()
+util = rdr.util()
+# Set util debug to true - it will print what's going on
+util.debug = True
+
+while True:
+    # Wait for tag
+    print("Waiting for tag...")
+    rdr.wait_for_tag()
+
+    # Request tag
+    (error, data) = rdr.request()
+    if not error:
+        print("Detected UID:")
+
+        (error, uid) = rdr.anticoll()
+       	print(uid)
+
+rdr.cleanup()
