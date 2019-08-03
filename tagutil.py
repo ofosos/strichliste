2033dc70b485349190e8900e57e2a55f06615184
Mark Meyer <mark@ofosos.org>
2019-08-03 16:46:36 +0200

    Add a quiet argument

--- tagutil.py
+++ tagutil.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python3
-from pirc522 import RFID
+
+import argparse
 import signal
 import time
+
+from pirc522 import RFID
+
+
+parser = argparse.ArgumentParser(description='Read an RFID tag.')
+parser.add_argument('--quiet', action='store_true')
+
+args = parser.parse_args()
+
+quiet = args.quiet
 
 rdr = RFID()
 util = rdr.util()
 # Set util debug to true - it will print what's going on
 util.debug = True
 
-print("Waiting for tag...")
+if not quiet:
+    print("Waiting for tag...")
 rdr.wait_for_tag()
 
 # Request tag
 (error, data) = rdr.request()
 if not error:
-    print("\nDetected: " + format(data, "02x"))
-    print("Detected UID:")
+    if not quiet:
+        print("\nDetected: " + format(data, "02x"))
+        print("Detected UID:")
 
     (error, uid) = rdr.anticoll()
     if not error:
         # Print UID
-        print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
-    else:
-        print("Error reading card:")
+        uid = (uid[0] << 24) + (uid[1] << 16) + (uid[2] << 8) + uid[3]
+        print(str(uid))
+    elif not quiet:
+        print("ERROR reading card:")
         print(error)
 rdr.cleanup()
